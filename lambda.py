61f9c01cbab90143deafbf1a3a3e6bc45ed026be
niteshmauryac7r <110805456+niteshmauryac7r@users.noreply.github.com>
2025-03-25 12:59:22 +0530

    "New Edit"

--- lambda.py
+++ lambda.py
@@ -1,3 +1,9 @@
 double  = lambda x : x*2
-
+cube  = lambda x: x*x*x
 print(double(2))
+print(cube(4))
+
+def apply(f,value1,value2,value3):
+    return 6 + f(value1,value2,value3)
+
+print(apply(lambda x,y,z: (x+y+z)/3 ,1,2,3 ))
