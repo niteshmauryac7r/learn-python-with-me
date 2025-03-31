629500740a5ce2bf29e029e3411fc6ea932f9ca5
niteshmauryac7r <110805456+niteshmauryac7r@users.noreply.github.com>
2025-03-31 17:47:13 +0530

    New update

---
+++ map_filter_reduce.py
@@ -0,0 +1,17 @@
+#MAP
+def cube(num):
+    return num*num*num
+
+l = [1,2,3,4]
+
+newl = list(map(cube,l))
+
+print(newl)
+
+# FILTER
+
+def greater(num):
+    return num > 2
+
+newnewl = list(filter(greater, l))
+print(newnewl)
